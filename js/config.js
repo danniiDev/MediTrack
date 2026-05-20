6e3df9c449126f442dd4bb3bc8299bda6eab54ec
danniiDev <20243110398@pampangastateu.edu.ph>
2026-05-21 06:23:36 +0800

    update production api

--- js/config.js
+++ js/config.js
@@ -1,4 +1,7 @@
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isLocal =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
+
 const API = isLocal
   ? 'http://localhost:3000/api'
-  : 'meditrack-production-a3dd.up.railway.app';
+  : 'https://meditrack-production-a3dd.up.railway.app/api';
